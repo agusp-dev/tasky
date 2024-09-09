fd63ce12a34337ae87783d4043ffe2ca3dc615da
Agustin <perezp.agustin@gmail.com>
2024-09-08 21:04:02 -0300

    feat: add remove task flow

--- src/ui/components/TaskItem/index.tsx
+++ src/ui/components/TaskItem/index.tsx
@@ -1,22 +1,29 @@
 import { useMemo } from 'react'
 import { Field, Checkbox, Label } from '@headlessui/react'
-import { CheckIcon } from '@heroicons/react/16/solid'
+import { CheckIcon, XMarkIcon } from '@heroicons/react/16/solid'
 import { TaskItem as TaskItemType, TaskStatus } from '~/types'
 import { TASK_STATUSES } from '~/constants'
 
 type Props = {
   task: TaskItemType,
   // eslint-disable-next-line no-unused-vars
-  onToggleStatus: (id: string, status: TaskStatus) => void
+  onToggleStatus: (id: string, status: TaskStatus) => void,
+  // eslint-disable-next-line no-unused-vars
+  onRemove: (id: string) => void,
 }
 
-export const TaskItem: React.FC<Props> = ({ task, onToggleStatus }) => {
+export const TaskItem: React.FC<Props> = ({ task, onToggleStatus, onRemove }) => {
 
   const onTaskChecked = (checked: boolean) => {
     onToggleStatus(task?.id, checked ? TASK_STATUSES.COMPLETED : TASK_STATUSES.PENDING)
   }
 
   const isChecked = useMemo(() => task?.status === TASK_STATUSES.COMPLETED, [task?.status])
+
+  const handleRemove = () => {
+    if (!task?.id) return
+    onRemove(task?.id)
+  }
 
   return (
     <li className='py-3'>
@@ -29,10 +36,17 @@ export const TaskItem: React.FC<Props> = ({ task, onToggleStatus }) => {
           {isChecked && <CheckIcon className='fill-purple-700' />}
         </Checkbox>
         <Label 
-          className={ `text-slate-800 cursor-pointer ${ isChecked ? 'font-light line-through' : 'font-normal' }` }
+          className={ `flex-1 text-slate-800 cursor-pointer 
+            ${ isChecked ? 'font-light line-through' : 'font-normal' }` }
         >
           { task?.title }
         </Label>
+        <button 
+          className='p-2 rounded-full hover:bg-violet-100'
+          onClick={ handleRemove }
+        >
+          <XMarkIcon className='size-5 fill-gray-400' />
+        </button>
       </Field>
     </li>
   )
